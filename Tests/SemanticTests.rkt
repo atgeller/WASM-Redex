81c26ed0d1d144d117be95bffcacb856a81c6dfe
Justin Frank <justinpfrank@protonmail.com>
2020-06-09 13:10:06 -0700

    Uncomment summation test as it now works

--- Tests/SemanticTests.rkt
+++ Tests/SemanticTests.rkt
@@ -260,7 +260,7 @@
                     ((i32 const 3)))))
   
 
-  #;(test-->>E -> ;; loop, if
+  (test-->>E -> ;; loop, if
                (term ((((((0 (() (func ((i32) -> (i32))
                                        (local (i32) ((loop (() -> (i32))
                                                            ((get-local 0)
